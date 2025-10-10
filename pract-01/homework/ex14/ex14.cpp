778e61ddca127dd29354ac17ff525184d4bd9851
Yordan Dimitrov <yorddimitrov1308@gmail.com>
2025-10-10 20:20:15 +0300

    14-18 homework

---
+++ pract-01/homework/ex14/ex14.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    int time;
+	std::cin >> time;
+
+	int hours = time / 3600;
+	int minutes = (time % 3600) / 60;
+	int seconds = time % 60;
+	std::cout << hours << " hours, " << minutes << " minutes, " << seconds << " seconds" << std::endl;
+
+	return 0;
+}
---
+++ pract-01/homework/ex17/ex17.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    int rent;
+	std::cin >> rent;
+
+	double cake = rent * 0.20;
+	double drinks = cake * 0.55;
+	double animator = rent / 3.0;
+
+	double total = rent + cake + drinks + animator;
+
+	std::cout << "Total budget: " << total << std::endl;
+
+	return 0;
+}
