673a61323dd8ba4137ce01e7a28d7ac83cc142cc
Yordan Dimitrov <yorddimitrov1308@gmail.com>
2025-10-17 16:26:27 +0300

    Adding sem 02 homework

---
+++ sem-02/homework/ex2/ex2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+	int num1, num2, num3, num4, num5;
+	std::cin >> num1 >> num2 >> num3 >> num4 >> num5;
+
+	if (((num2 >= num3 && num2 >= num1) || (num2 <= num3 && num2 <= num1))
+		&& ((num3 >= num4 && num3 >= num2) || (num3 <= num4 && num3 <= num2))
+		&& ((num4 >= num3 && num4 >= num5) || (num4 <= num3 && num4 <= num5))) {
+
+		std::cout << "yes" << std::endl;
+	}
+	else {
+		std::cout << "no" << std::endl;
+	}
+
+	return 0;
+}
