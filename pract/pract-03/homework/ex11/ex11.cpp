da31b9bc5e20c0f90c555790e96213b2bbbdbdc1
Yordan Dimitrov <yorddimitrov1308@gmail.com>
2025-10-25 17:56:21 +0300

    adding prac03 homework

---
+++ pract/pract-03/homework/ex11/ex11.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main()
+{
+	char startSetA, endSetA, startSetB, endSetB, testLetter;
+	std::cin >> startSetA >> endSetA >> startSetB >> endSetB >> testLetter;
+
+	startSetA += 'a' - 'A';
+	endSetA += 'a' - 'A';
+	if(testLetter >= 'A' && testLetter <= 'Z') testLetter += 'a' - 'A';
+
+
+	bool isInUnion = false, isInIntersection = false, isInDifference = false;
+
+
+	if ((testLetter >= startSetA && testLetter <= endSetA) ||
+		(testLetter >= startSetB && testLetter <= endSetB)) {
+		isInUnion = true;
+	}
+
+	if (testLetter >= startSetA && testLetter <= endSetA &&
+		testLetter >= startSetB && testLetter <= endSetB) {
+		isInIntersection = true;
+	}
+
+	if (!isInIntersection && testLetter >= startSetA && testLetter <= endSetA) {
+		isInDifference = true;
+	}
+
+	bool isPartOfOnlyOneSet = isInUnion && !isInIntersection;
+
+	std::cout << isInUnion << ", " << isInIntersection << ", " << isInDifference << ", " << isPartOfOnlyOneSet;
+
+    return 0;
+}
---
+++ pract/pract-03/homework/ex12/ex12.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+
+int main()
+{
+	char figure, startPosLetter, endPosLetter;
+	int startPosNumber, endPosNumber;
+	std::cin >> figure >> startPosLetter >> startPosNumber >> endPosLetter >> endPosNumber;
+
+	if(startPosLetter < 'a' || startPosLetter > 'h' || endPosLetter < 'a' || endPosLetter > 'h' ||
+	   startPosNumber < 1 || startPosNumber > 8 || endPosNumber < 1 || endPosNumber > 8) {
+		std::cout << "Invalid input";
+		return 0;
+	}
+
+	if(startPosLetter == endPosLetter && startPosNumber == endPosNumber) {
+		std::cout << "Invalid move";
+		return 0;
+	}
+
+	
+	switch (figure) {
+		case 'P':
+			if(startPosLetter == endPosLetter && abs(endPosNumber - startPosNumber) == 1) {
+				std::cout << "Valid move";
+			}
+			else {
+				std::cout << "Invalid move";
+			}
+			break;
+		case 'K':
+			if((abs(endPosLetter - startPosLetter) == 1 && abs(endPosNumber - startPosNumber) == 2) ||
+			   (abs(endPosLetter - startPosLetter) == 2 && abs(endPosNumber - startPosNumber) == 1)) {
+				std::cout << "Valid move";
+			} 
+			else {
+				std::cout << "Invalid move";
+			}
+			break;
+		case 'B':
+			if(abs(endPosLetter - startPosLetter) == abs(endPosNumber - startPosNumber)) {
+				std::cout << "Valid move";
+			} 
+			else {
+				std::cout << "Invalid move";
+			}
+			break;
+		case 'R':
+			if(startPosLetter == endPosLetter || startPosNumber == endPosNumber) {
+				std::cout << "Valid move";
+			} else {
+				std::cout << "Invalid move";
+			}
+			break;
+		case 'Q':
+			if (startPosLetter == endPosLetter || startPosNumber == endPosNumber ||
+				abs(endPosLetter - startPosLetter) == abs(endPosNumber - startPosNumber)) {
+				std::cout << "Valid move";
+			}
+			else {
+				std::cout << "Invalid move";
+			}
+			break;
+		case 'X':
+			if ((abs(startPosLetter - endPosLetter) == 1 && abs(startPosNumber - endPosNumber) == 1) ||
+				(startPosLetter == endPosLetter && abs(startPosNumber - endPosNumber) == 1)) {
+				std::cout << "Valid move";
+			}
+			else {
+				std::cout << "Invalid move";
+			}
+			break;
+	}
+
+	return 0;
+}
