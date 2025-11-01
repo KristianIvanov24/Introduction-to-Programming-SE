cd0d613b89cb3a52dd72df48f2a76afcd85c7a88
K0n43t0 <116839877+K0n43t0@users.noreply.github.com>
2025-11-01 22:18:41 +0300

    added ex05

---
+++ sem-04/zad05.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+//Напишете функция, която приема естествено число и връща ново число, което има същите цифри, но в сортиран вид.
+int sortDigits(int num)
+{
+	int result = 0;
+	for (int i = 1; i <= 9; i++)
+	{
+		int temp = num;
+		while (temp > 0)
+		{
+			if (temp % 10 == i)
+			{
+				result *= 10;
+				result += i;
+			}
+			temp /= 10;
+		}
+		
+	}
+return result;
+}
+int main()
+{
+	int num;
+	std::cout << "Input number: ";
+	std::cin >> num;
+	std::cout << sortDigits(num);
+}
