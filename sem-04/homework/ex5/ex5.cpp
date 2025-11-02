00655843a1e5d1c8eb1b07f789b0e95ecc178704
Yordan Dimitrov <yorddimitrov1308@gmail.com>
2025-11-02 11:54:08 +0200

    adding sem04 homework

---
+++ sem-04/homework/ex5/ex5.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+int getSmallestDigitInNumber(int num)
+{
+    int smallestDigit = 9;
+    while (num > 0) {
+        int currentDigit = num % 10;
+        if (currentDigit < smallestDigit) {
+            smallestDigit = currentDigit;
+        }
+        num /= 10;
+    }
+    return smallestDigit;
+}
+
+int removeDigitFromNumber(int num, int digitToRemove)
+{
+    int result = 0;
+    int multiplier = 1;
+    bool removed = false;
+
+    while (num > 0) {
+        int currentDigit = num % 10;
+        num /= 10;
+
+        if (currentDigit == digitToRemove && !removed) {
+            removed = true;
+            continue;
+        }
+
+        result += currentDigit * multiplier;
+        multiplier *= 10;
+    }
+
+    return result;
+}
+
+int getNumberWithSortedDigits(int num)
+{
+	int numWithSortedDigits = 0;
+    while(num > 0) {
+		int smallestDigitInNumber = getSmallestDigitInNumber(num);
+		numWithSortedDigits = numWithSortedDigits * 10 + smallestDigitInNumber;
+
+		num = removeDigitFromNumber(num, smallestDigitInNumber);
+	}
+
+	return numWithSortedDigits;
+}
+
+int main()
+{
+    int num;
+	std::cin >> num;
+
+    if(num < 0) {
+        std::cout << "Invalid input";
+        return 0;
+	}
+
+	std::cout << getNumberWithSortedDigits(num);
+
+    return 0;
+}
