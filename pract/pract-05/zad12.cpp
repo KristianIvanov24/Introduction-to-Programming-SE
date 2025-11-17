c87db1e93011562012652db8d97adfbb22563467
K0n43t0 <116839877+K0n43t0@users.noreply.github.com>
2025-11-17 23:12:34 +0300

    added ex12

---
+++ pract/pract-05/zad12.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+//Напишете функция, която приема неотрицателни числа n и k и връща дали k е инфикс в n.
+const int MIN_VALUE = 100;
+bool isInfix(int n, int k)
+{
+	if (n < 100)
+		return false;
+	n /= 10;
+	int temp = k;
+	while (n > 10)
+	{
+		if (k != 0 && temp == 0)
+			return true;
+		if (temp % 10 == n % 10)
+		{
+			temp /= 10;
+			n /= 10;
+		}
+		else
+		{
+			temp = k;
+			n /= 10;
+		}
+	}
+	return k != 0 && temp == 0;
+}
+int main()
+{
+	int n, k;
+	std::cout << "Input 2 numbers: ";
+	std::cin >> n >> k;
+	std::cout << isInfix(n, k);
+}
